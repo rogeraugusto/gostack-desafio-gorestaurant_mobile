005bbedfb076efaa2ea018b466fb22c93bfaa699
Roger Moraes <roger.lm.mr@gmail.com>
2020-07-06 21:36:18 -0300

    GoRestaurant Mobile Challange. All tests has passed

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,7 +32,14 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get('/favorites');
+
+      const favoritesFormatted = response.data.map((favorite: Food) => ({
+        ...favorite,
+        formattedPrice: formatValue(favorite.price),
+      }));
+
+      setFavorites(favoritesFormatted);
     }
 
     loadFavorites();
